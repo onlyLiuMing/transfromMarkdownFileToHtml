f07f90e110069866506e19531259efb9d69e6fa3
王萌 <only_liuming@sina.com>
2018-05-23 09:46:17 +0800

    修改对应id错误；左侧list改为可两级下拉

--- index.js
+++ index.js
@@ -1,64 +1,131 @@
-const marked = require('marked');
-const fs = require('fs');
+const marked = require( 'marked' );
+const fs = require( 'fs' );
 
+// list 部分的解析方法
 let renderer = new marked.Renderer();
-renderer.listitem = function (body) {
-  return `
-    <li><a href="#${body}"><i class="fa fa-angle-right"></i>${body}</a></li>
-  `
+renderer.listitem = function ( body ) {
+	let content, title;
+	if ( ( /<.+?>/ig ).test( body ) ) {
+		content = body.slice( body.indexOf( '<' ) );
+		title = body.slice( 0, body.indexOf( '<' ) );
+		return `<li><a href="#${title}">${title}<span class="category-widget-btn"></span></a>${content}</li>`
+	} else {
+		content = body;
+		title = body;
+		return `<li><a href="#${title}" ><i class="fa fa-angle-right"></i>${content}</a></li>`
+	}
 }
-renderer.list = function (body, isorder) {
-  const tag = isorder
-    ? 'ol'
-    : 'ul';
-  return `
-    <${tag} class='links'>
-      ${body}
-    </${tag}>`
+renderer.list = function ( body, isorder ) {
+	return `<ul ${isorder?'':'id="category-widget"'}>${body}</ul>`
 }
 
-const content = marked(fs.readFileSync('./markdown/content.md').toString());
-const list = marked(fs.readFileSync('./markdown/list.md').toString(), {renderer: renderer});
-const body = fillBody(fillList(list), fillContent(content));
+// content部分的解析方法
+let parse_content = new marked.Renderer();
+parse_content.heading = function ( body, level ) {
+	return `
+          <h${level} id='${body}'>
+            ${body}
+          </h${level}>`;
+}
+
+// 生成对应html结构
+const content = marked( fs.readFileSync( './markdown/content.md' ).toString(), {
+	renderer: parse_content
+} );
+const list = marked( fs.readFileSync( './markdown/list.md' ).toString(), {
+	renderer: renderer
+} );
+const body = fillBody( fillList( list ), fillContent( content ) );
 
 // 写入文件
-fs.writeFile('./output/document.html', body, err => {
-  if (err) {
-    console.log('产生了错误', err);
-    process.exit(1);
-    return false;
-  }
-  console.log('文件生成成功');
-  process.exit();
-});
+fs.writeFile( './output/document.html', body, err => {
+	if ( err ) {
+		console.log( '产生了错误', err );
+		process.exit( 1 );
+		return false;
+	}
+	console.log( '文件生成成功' );
+	process.exit();
+} );
+
+
+/**************** 生成html方法 *****************/
 
 // 填充content
-function fillContent(content = '') {
-  return `
-  <div class="col-md-9 col-md-push-3">
+function fillContent( content = '' ) {
+	return `
+  <div class="col-md-8 col-md-push-4">
     ${content}
   </div> `
 }
 // 填充list
-function fillList(list = '') {
-  return `
-  <aside class="col-md-3 col-md-pull-9 sidebar" role="complementary">
+function fillList( list = '' ) {
+	return `
+  <aside class="col-md-3 col-md-pull-8 sidebar" role="complementary">
     <div class="widget">
       ${list}
     </div>
   </aside>`
 }
 // 填充body
-function fillBody(list = '', content = '') {
-  const result = `
-    <div class='container'>
+function fillBody( list = '', content = '' ) {
+	const result = `
+  {% extends "base.html" %}
+  {% block content%}
+  {% load i18n %}
+  <style>
+    aside.sidebar li{list-style:none}
+    #category-widget > li > a {
+      color: #666;
+      font-size: 16px;
+      position: relative;
+      border-top: 1px solid #eaeaea;
+      padding: 6px 10px 6px 43px;
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+    #category-widget > li > a > i{
+      display:none;
+    }
+    #category-widget li li a {
+      color: #7e7e7e;
+      padding: 2px 10px 2px 48px;
+      font-weight:normal;
+    }
+    #category-widget a {
+      display: block;
+      font: 600 12px/24px 'Open Sans', Verdana;
+      text-transform: uppercase;
+    }
+  </style>
+  <div class="o-head-bottom"></div><!-- space -->
+  <div class="mb50"></div>
+  <div id="content" role="main">
+    <div class='container' >
       <div class='row'>
         ${content}
-
         <div class="mb60 clearfix visible-sm visible-xs"></div><!-- space -->
-
         ${list}
       </div>
-    </div>`;
-  return result;
+    </div>
+  </div>
+  <script>
+    $('.category-widget-btn').on('click', function (e) {
+      var $this = $(this),
+        parent= $this.closest('li');
+
+      if (parent.hasClass('open')) {
+        parent.find('ul').slideUp(400, function() {
+          parent.removeClass('open');
+        });
+      } else {
+        parent.find('ul').slideDown(400, function() {
+          parent.addClass('open');
+        });
+      }
+      e.preventDefault();
+    });
+  </script>
+  {%endblock%}`;
+	return result;
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,20 @@
 - list.md是左侧导航栏
 - content.md是右侧内容
 - list中的名字必须与content中的内容相对应
+
+环境
+-----
+- 需要node > 9.0（安装最新的就行了）
+- 可以选择用yarn管理包，自带的npm也可以
+
+安装步骤
+-----
+1. 去node官网下载最新的node（https://nodejs.org/zh-cn/ ）
+2. 将node中的/node_path/bin/node 和 /node_path/bin/npm 链接到/usr/local/bin目录下（设为全局变量也可以）
+3. 在此目录下（项目目录）执行 `npm install`
+
+使用
+-----
+1. 将所需的markdown文件放置./markdown/文件夹下
+2. 执行 `npm run start`
+3. 目标文件将输出在./output文件夹下
